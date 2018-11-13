775f82a4c98c1af6df4df619ad5394a7ab97cbe2
Carlo Mazzaferro <carlo.mazzaferro@bytecubed.com>
2018-11-13 01:49:45 -0500

    Bump version: 0.1.0 → 0.2.0

--- racket/__init__.py
+++ racket/__init__.py
@@ -5,6 +5,6 @@ from racket.models.learner import Learner, KerasLearner
 
 __author__ = """Carlo Mazzaferro"""
 __email__ = 'carlo.mazzaferro@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
