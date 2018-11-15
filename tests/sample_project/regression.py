b1c2187df60acb0a3eb37c75fcc417734f5608db
Carlo Mazzaferro <carlo.mazzaferro@bytecubed.com>
2018-11-15 17:59:32 -0500

    Naming change on sample files

--- tests/sample_project/regression.py
+++ tests/sample_project/regression.py
@@ -6,16 +6,10 @@ from racket.models.learner import KerasLearner
 from sklearn.model_selection import train_test_split
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import SimpleRNN, Dense
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.metrics import mse
+from tensorflow.keras.metrics import mae
 from time import time
-
-from tensorflow_serving.apis import model_service_pb2
-from tensorflow_serving.apis import model_service_pb2_grpc
-from tensorflow_serving.config import model_server_config_pb2
-from tensorflow_serving.util import status_pb2
-from tensorflow_serving.apis import model_management_pb2
 
 
 tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
@@ -25,7 +19,7 @@ class KerasModel(KerasLearner):
 
     VERSION = '1.1.1'
     MODEL_TYPE = 'regression'
-    MODEL_NAME = 'keras-complex-lstm'
+    MODEL_NAME = 'keras-simple-regression'
 
     def build_model(self):
         optimizer = tf.train.RMSPropOptimizer(0.001)
@@ -33,7 +27,7 @@ class KerasModel(KerasLearner):
         model.add(Dense(24, input_dim=5, kernel_initializer='normal', activation='relu'))
         model.add(Dense(48, kernel_initializer='normal', activation='relu'))
         model.add(Dense(1, kernel_initializer='normal'))
-        model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=[mse])
+        model.compile(loss='mean_absolute_error', optimizer=optimizer)
         return model
 
     def fit(self, x, y, x_val=None, y_val=None, epochs=2, batch_size=20):
@@ -45,4 +39,4 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(x, y)
     kf = KerasModel()
     kf.fit(X_train, y_train, x_val=X_test, y_val=y_test)
-    kf.store()
+    kf.store(autoload=True)
