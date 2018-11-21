17eb76ad746a66a3c2aa65f36f4aa64cbc13e313
Carlo Mazzaferro <carlo.mazzaferro@bytecubed.com>
2018-11-21 01:02:23 -0500

    minor fixes for coveralls

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 python setup.py sdist bdist_wheel
 pip install twine
-twine upload --username Mazzafish --password $PYPI dist/*
+twine upload --username Mazzafish --password $PYPI --skip-existing dist/*
