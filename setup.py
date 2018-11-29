36882ba3c9c2717f2062bf257b7aced684ca4872
Carlo Mazzaferro <carlo.mazzaferro@bytecubed.com>
2018-11-29 14:27:56 -0500

    Bump version: 0.3.5 → 0.3.6

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     test_suite='tests',
     tests_require=test_requires,
     url='https://github.com/carlomazzaferro/racket',
-    version='0.3.5',
+    version='0.3.6',
     zip_safe=False,
 )
