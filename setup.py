48fe38c679d4f5cc046bf9647cfe0f8352be0871
carlomazzaferro <carlo.mazzaferro@gmail.com>
2019-06-06 20:51:21 -0400

    Bump version: 0.3.7 → 0.3.8

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite='tests',
     tests_require=test_requires,
     url='https://github.com/carlomazzaferro/racket',
-    version='0.3.7',
+    version='0.3.8',
     zip_safe=False,
 )
