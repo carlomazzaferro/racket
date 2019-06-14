2dac58697a42c2fcfadef14f880d75563420c590
carlomazzaferro <carlo.mazzaferro@gmail.com>
2019-06-14 14:00:52 -0400

    Updated web components for templates

---
+++ racket/template/web/precache-manifest.3be394996438461aed1dfe1534b1c647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a644a764befe1551fbfb06ce1cc828ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c77fcab2749c106a17e3",
+    "url": "/static/css/2.280c789f.chunk.css"
+  },
+  {
+    "revision": "e3f89eb99fd4a3c5b0c2",
+    "url": "/static/css/main.4cc9d3bc.chunk.css"
+  },
+  {
+    "revision": "c77fcab2749c106a17e3",
+    "url": "/static/js/2.2580a9e0.chunk.js"
+  },
+  {
+    "revision": "e3f89eb99fd4a3c5b0c2",
+    "url": "/static/js/main.816293ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
